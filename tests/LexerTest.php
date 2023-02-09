c1295e5af44b3bd4745abfd29a2d61db4825da44
NickSettler <moiseevnikita14@gmail.com>
2023-02-09 14:49:52 +0100

    feat(tests): add test cases

--- tests/LexerTest.php
+++ tests/LexerTest.php
@@ -4,10 +4,51 @@ declare(strict_types=1);
 
 namespace tests;
 
-class LexerTest extends \PHPUnit\Framework\TestCase
+use E_LEXER_TOKENS;
+use Exception;
+use LexicalAnalysis;
+use LexicalToken;
+use PHPUnit\Framework\TestCase;
+
+class LexerTest extends TestCase
 {
-    public function test(): void
+    private function getLexer(string $input): LexicalAnalysis
     {
-        $this->assertTrue(true);
+        return new LexicalAnalysis($input);
+    }
+
+    private function testCaseProvider(): array
+    {
+        return [
+            [
+                'input'    => 'true false',
+                'expected' => [
+                    new LexicalToken('true', E_LEXER_TOKENS::KEYWORD_TRUE),
+                    new LexicalToken('false', E_LEXER_TOKENS::KEYWORD_FALSE),
+                ],
+            ],
+            [
+                'input'    => 'MOVE ADD',
+                'expected' => [
+                    new LexicalToken('MOVE', E_LEXER_TOKENS::COMMAND),
+                    new LexicalToken('ADD', E_LEXER_TOKENS::COMMAND),
+                ],
+            ],
+        ];
+    }
+
+    /**
+     * @dataProvider testCaseProvider
+     */
+    public function test(string $input, array $expected): void
+    {
+        $lexer = $this->getLexer($input);
+        $actual = [];
+        $token = $lexer->getNextToken();
+        while ($token->getType() !== E_LEXER_TOKENS::END_OF_FILE) {
+            $actual[] = $token;
+            $token = $lexer->getNextToken();
+        }
+        $this->assertEquals($expected, $actual);
     }
 }
