443350e17a7bd8901864a0d7a3a8dadd0639444c
NickSettler <moiseevnikita14@gmail.com>
2023-02-09 13:45:00 +0100

    feat(tests): add composer and basic test

---
+++ tests/LexerTest.php
@@ -0,0 +1,12 @@
+<?php
+
+declare(strict_types=1);
+
+namespace tests;
+
+class LexerTest extends \PHPUnit\Framework\TestCase
+{
+    public function test(): void {
+        $this->assertTrue(true);
+    }
+}
